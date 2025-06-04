c38dc6927cbbc468cb32b68a9209ce6181d29020
jedd <jed@jed.jed>
2025-06-04 11:59:32 +0200

    fix etape 6 (j'avais lu VISUELLE désolé) + ajout étape 5

--- main.c
+++ main.c
@@ -15,24 +15,33 @@ int main() {
         fprintf(stderr, "Erreur\n");
         return 1;
     }
-
     int nbImpostors = getNombre(nbJoueurs);
 
     initSession(nbJoueurs, nbImpostors, joueurs);
 
-    afficherJoueurs(0, nbJoueurs, joueurs);
-    
-    voterAleatoirement(nbJoueurs, joueurs);
+    while(nbImpostors!=0 || nbJoueurs-nbImpostors >= 2){ // dans l'etape 5 ligne 1 il dit "au moins deux joueurs" mais je pense la boucle va run à l'infini si je met au moins 2 joueurs alors je met au moins 2 crewmates
 
-    afficherJoueurs(0, nbJoueurs, joueurs);
+        afficherJoueurs(0, nbJoueurs, joueurs);
 
-    joueurs=supprimerJoueurAvecPlusDeVotes(joueurs, &nbJoueurs);
-    
-    afficherJoueurs(0, nbJoueurs, joueurs);
+        voterAleatoirement(nbJoueurs, joueurs);
 
-    printf("%i",nbJoueurs);
-    
-    system("clear");
+        afficherJoueurs(0, nbJoueurs, joueurs);
+
+        supprimerJoueurAvecPlusDeVotes(joueurs, &nbJoueurs);
+
+        afficherJoueurs(0, nbJoueurs, joueurs);
+
+        nbImpostors=recompterNombreImpostors(joueurs, nbJoueurs);
+
+    }
+
+    if(nbImpostors>=nbJoueurs-nbImpostors){
+        printf("Les impostors gagnent\n");
+    }else{
+        printf("Les crewmates gagnent\n");
+    }
+
+    free(joueurs);
     
     return 0;
 }
